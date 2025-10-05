19738b899c66c598ccc9d654d6e1fc226ee4182c
j3rrryy <pavryab@mail.ru>
2025-10-05 18:17:41 +0300

    chore: delete unused files

--- test1.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-response = requests.get(
-    "https://catalog.api.2gis.com/3.0/markers?q=кафе&point=37.545423%2C55.740693&radius=500&location=37.545423%2C55.740693&sort=distance&key=2dfc0b30-cdb7-4014-9142-4d101cb77824"
-)
-print(response.json())
