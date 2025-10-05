9700785da0d0b576bb0fbfeee9bab226817e0c20
j3rrryy <pavryab@mail.ru>
2025-10-05 05:32:48 +0300

    feat: result caching

--- src/backend/service/calculation_service.py
+++ src/backend/service/calculation_service.py
@@ -1,11 +1,13 @@
 from dataclasses import asdict
 
+from cashews import cache
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from dto import request as request_dto
 from dto import response as response_dto
 from repository import CalculationRepository
 from tasks import calculate
+from utils import get_calculation_key
 
 
 class CalculationService:
@@ -21,4 +23,14 @@ class CalculationService:
     async def get_calculation(
         calculation_id: str, session: AsyncSession
     ) -> response_dto.CalculationResponseDTO:
-        return await CalculationRepository.get_calculation(calculation_id, session)
+        cache_key = get_calculation_key(calculation_id)
+        if cached := await cache.get(cache_key):
+            return cached
+
+        calculation = await CalculationRepository.get_calculation(
+            calculation_id, session
+        )
+
+        if calculation.result:
+            await cache.set(cache_key, calculation, 3600)
+        return calculation
---
+++ src/backend/utils/utils.py
@@ -0,0 +1,2 @@
+def get_calculation_key(calculation_id: str) -> str:
+    return f"calculation:{calculation_id}"
